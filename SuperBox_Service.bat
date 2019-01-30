bd989e38cae3b33833367d4cfacc9734ae041c31
chen1880 <47165775+chen1880@users.noreply.github.com>
2019-01-30 11:10:09 +0800

    Add files via upload

---
+++ SuperBox_Service.bat
@@ -0,0 +1,9 @@
+@echo off 
+
+title=SuperBox_Service
+
+d:
+
+java -Dfile.encoding=UTF-8 -jar soft/SuperBox.jar 
+
+pause>nul 
---
+++ WatchDog_Service.bat
@@ -0,0 +1,9 @@
+@echo off 
+
+title=WatchDog_Service
+
+d:
+
+java -Dfile.encoding=UTF-8 -jar soft/WatchDog.jar 
+
+pause>nul 
